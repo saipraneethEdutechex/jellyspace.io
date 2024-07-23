106ed5bb3f07a058963958eff6630d7c7a52996e
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-07-23 15:37:36 +0530

    Add changes

--- jellyspace/src/app/new-user-register/new-user-register.component.ts
+++ jellyspace/src/app/new-user-register/new-user-register.component.ts
@@ -4,70 +4,45 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-user-register',
   templateUrl: './new-user-register.component.html',
-  styleUrls: ['./new-user-register.component.css']
+  styleUrls: ['./new-user-register.component.css'],
 })
 export class NewUserRegisterComponent implements OnInit {
-
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router) {}
 
   onRadioValue: any;
-  radiocheck: boolean =false;
-  isFreelance:boolean=false;
-  isCompany:boolean=false;
-  isBigCompany:boolean=false;
-
-  optinos = {opacity:2}
+  radiocheck: boolean = false;
+  isFreelance: boolean = false;
+  isCompany: boolean = false;
+  isBigCompany: boolean = false;
 
   ngOnInit(): void {
-    var b = localStorage.getItem("newuser");
-    switch (b) {
-      case "freelancer":
-        this.isFreelance=true;
+    const userType = localStorage.getItem('newuser');
+    switch (userType) {
+      case 'freelancer':
+        this.isFreelance = true;
         this.radiocheck = true;
         break;
-        case "organization":
-        this.isBigCompany=true;
+      case 'organization':
+        this.isBigCompany = true;
         this.radiocheck = true;
-          break;
-        case "company":
-        this.isCompany=true;
+        break;
+      case 'company':
+        this.isCompany = true;
         this.radiocheck = true;
-          break;
-
+        break;
       default:
         break;
     }
   }
 
-  onChnageValue(val:any,eve:any) {
-    console.log(eve)
-    console.log(eve.target.checked)
-    this.radiocheck = eve.target.checked
+  onChnageValue(val: any, event: any) {
+    this.radiocheck = event.target.checked;
     this.onRadioValue = val;
     localStorage.setItem('accountType', val);
   }
 
-  JoinJellySpace() {
-    if(this.radiocheck === true) {
-      if(this.onRadioValue === 'freelancer') {
-        this.router.navigate(['user-details']);
-        localStorage.setItem("newuser","freelancer");
-      } else if(this.onRadioValue === 'organization') {
-        this.router.navigate(['company-list']);
-        localStorage.setItem("newuser","organization");
-      }
-      else {
-        this.router.navigate(['company-list']);
-        localStorage.setItem("newuser", "company")
-      }
-    } else {
-
-
-      // alert("Please Select At Least One Option And Continue...")
-    }
-
+  startFreeTrial() {
+    // Redirect to company listing or desired page
+    this.router.navigate(['company-list']);
   }
-
 }
